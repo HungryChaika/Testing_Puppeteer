2c542c9a9526a40c70e6bf8e76a4bc5128afaee6
HungryChaika <vologzhanin_00@mail.ru>
2022-05-28 20:27:21 +0400

    clicking on all elements, getting information and going to the main page (without navigating through the pages)

--- scrape.js
+++ scrape.js
@@ -8,18 +8,29 @@ let scrape = async () => {
     await page.goto("http://books.toscrape.com/");
     await page.waitForTimeout(1000);
 
-    await page.click('#default > div > div > div > div > section > div:nth-child(2) > ol > li:nth-child(1) > article > div.image_container > a > img');
-    const result = await page.evaluate(() => {
-        let title = document.querySelector('h1').innerText;
-        let price = document.querySelector('.price_color').innerText;
-        return {
-            title,
-            price
-        }
+    const num = await page.evaluate(() => {
+        let articles = document.querySelectorAll('.product_pod');
+        return articles.length;
     });
 
+    const books = [];
+    
+    for(let i = 1; i <= num; i++) {
+        await page.click(`#default > div > div > div > div > section > div:nth-child(2) > ol > li:nth-child(${i}) > article > div.image_container > a > img`);
+        const book = await page.evaluate(() => {
+            let title = document.querySelector('h1').innerText;
+            let price = document.querySelector('.price_color').innerText;
+            return {
+                title,
+                price
+            }
+        });
+        books.push(book);
+        await page.goBack();
+    };
+
     browser.close();
-    return result;
+    return books;
 };
 
 scrape().then((value) => {
